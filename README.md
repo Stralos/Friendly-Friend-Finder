2d0de66a62c329156b5018856c47f734503e7d1b
Stralos <paulius.panda@gmail.com>
2017-02-27 01:51:57 +0200

    added url loader and file loader for image loading in css changed eslint config to tab lenght 2

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 Friendly-Friend-Finder
+
+https://www.npmjs.com/package/file-loader file loader used for managing files included in css. Bundels them into files in dist
+https://www.npmjs.com/package/url-loader url loader simmilar to file loader, but instead of creating files just gets their url.
+
+if I want source maps and background images i need to use public path(to server or local) or remove source map https://github.com/webpack-contrib/css-loader/issues/29
--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,29 @@
-'use strict';
+"use strict";
 
-var path = require('path');
+var path = require("path");
 
 var config = {
-  devtool: 'eval-source-map',
-  entry: path.join(__dirname, '/src/index.jsx'),
+  devtool: "eval-source-map",
+  entry: path.join(__dirname, "/src/index.jsx"),
   output: {
-    path: path.join(__dirname, '/dist/'),
-    filename: 'bundle.js'
+    path: path.join(__dirname, "/dist/"),
+    publicPath: path.join(__dirname, "/").replace(/\\/g, "/"),
+    filename: "bundle.js"
   },
   module: {
     loaders: [
       {
         test: /.jsx?$/,
-        loader: 'babel-loader',
+        loader: "babel-loader",
         exclude: /node_modules/
       },
       {
         test: /\.less$/,
-        loaders: ['style-loader', 'css-loader?sourceMap&modules&localIdentName=[local]---[hash:base64:5]', 'less-loader?sourceMap']
+        loaders: ["style-loader", "css-loader?sourceMap&modules&localIdentName=[local]---[hash:base64:5]", "less-loader?sourceMap"]
+      },
+      { 
+        test: /\.(jpg|gif|png|svg)$/, 
+        loader: `url-loader?limit=${65536}&mimetype=image/[ext]&name=[path][name].[ext]`
       }
     ]
   }
