e32e32bf9f25009e003c16b12a90eb8ee9aeb02e
Stralos <paulius.panda@gmail.com>
2017-02-26 23:02:39 +0200

    fixed linter and linter errors

--- components/main/main.jsx
+++ components/main/main.jsx
@@ -1,19 +1,19 @@
-import React from 'react';
-import AboutMe from '../aboutMe/aboutMe.jsx';
-import Galery from '../galery/galery.jsx';
+import React from "react";
+import AboutMe from "../aboutMe/aboutMe.jsx";
+import Galery from "../galery/galery.jsx";
 
-import css from '../../style/main.less';
+import css from "../../style/main.less";
 
 class Main extends React.Component {
-  render () {
-    return (
+	render () {
+		return (
         <div>
             <h1 className={css.header}>Test</h1>
             <AboutMe/>
             <Galery/>
         </div>
-    )
-  }
+		);
+	}
 }
 
-export default Main
+export default Main;
--- src/index.jsx
+++ src/index.jsx
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Main from '../components/main/main.jsx';
 
-ReactDOM.render(<Main/>, document.getElementById('hello'))
+ReactDOM.render(<Main/>, document.getElementById('application'))
