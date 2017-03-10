acdf3820b47df4ab07ae49f220117b1bf34a4e53
Stralos <paulius.panda@gmail.com>
2017-03-10 16:40:40 +0200

    removed empty style file.

--- components/main/main.jsx
+++ components/main/main.jsx
@@ -2,8 +2,6 @@ import React from "react";
 import AboutMe from "../aboutMe/aboutMe.jsx";
 import Galery from "../galery/galery.jsx";
 
-import css from "./style.less";
-
 class Main extends React.Component {
   render () {
     return (
