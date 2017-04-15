c162e4d1485e21f2e73f4a956bdafa335a204da7
Stralos <paulius.panda@gmail.com>
2017-04-15 16:56:08 +0300

    refactored main component.

--- components/main/main.jsx
+++ components/main/main.jsx
@@ -1,15 +1,27 @@
 import React from "react";
+import ReactDOM from "react-dom";
 import AboutMe from "../aboutMe/aboutMe.jsx";
 import Galery from "../galery/galery.jsx";
 import Footer from "../footer/footer.jsx";
 import ContactMe from "../contactMe/contactMe.jsx";
+import Intro from "../intro/intro.jsx";
 
 class Main extends React.Component {
+  constructor(){
+    super();
+    this.scrollToAboutMe = this.scrollToAboutMe.bind(this);
+  }
+
+  scrollToAboutMe(){
+    ReactDOM.findDOMNode(this.aboutMeDOM).scrollIntoView({ behavior: "smooth" });
+  }
+
   render () {
     return (
         <div>
-            <Galery/>
-            <AboutMe/>
+            <Intro event={this.scrollToAboutMe}/>
+        
+            <AboutMe ref={component => this.aboutMeDOM = component} />
             <ContactMe/>
             <Footer/>
         </div>
